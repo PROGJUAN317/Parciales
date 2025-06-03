d18ba7e47f4fde58815a0da1aee31d7577f9741e
Juan Sebastian Navarrete <163350568+PROGJUAN317@users.noreply.github.com>
2025-06-03 13:03:40 +0000

    Add initial implementation of main.cpp with a greeting message

---
+++ parcial_2/build/CMakeFiles/parcial_2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/parcial_2.dir/main.cpp.o"
+  "parcial_2"
+  "parcial_2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/parcial_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
